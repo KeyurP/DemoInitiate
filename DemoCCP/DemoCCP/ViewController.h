011f7a610dea3278dab44f2ea401a24551eaa096
Keyur Prajapati <macmini19@Keyur-Mac-Mini.local>
2014-02-05 15:37:54 +0530

    k,hjklj

---
+++ DemoCCP/DemoCCP/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DemoCCP
+//
+//  Created by Keyur Prajapati on 05/02/14.
+//  Copyright (c) 2014 macmini19. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ DemoCCP/DemoCCPTests/DemoCCPTests.m
@@ -0,0 +1,34 @@
+//
+//  DemoCCPTests.m
+//  DemoCCPTests
+//
+//  Created by Keyur Prajapati on 05/02/14.
+//  Copyright (c) 2014 macmini19. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DemoCCPTests : XCTestCase
+
+@end
+
+@implementation DemoCCPTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
